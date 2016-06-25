9617ed3e647a3507d707ff2095209d2401c1ead7
Marcin Dulak <marcin.dulak@gmail.com>
2016-06-25 21:45:20 +0200

    ask question01.txt

--- run01.sh
+++ run01.sh
@@ -20,12 +20,13 @@ vagrant ssh node-1 -c "sudo su - -c 'sbd -d /dev/sdb1 dump'"
 
 # Enable stonith:
 vagrant ssh node-1 -c "sudo su - -c 'pcs stonith list'"
+vagrant ssh node-1 -c "sudo su - -c 'pcs stonith create MyStonith fence_sbd devices=/dev/sdb1 power_timeout=21 action=off'"
 vagrant ssh node-1 -c "sudo su - -c 'pcs property set stonith-enabled=true'"
 # https://www.suse.com/documentation/sle_ha/book_sleha/data/sec_ha_storage_protect_fencing.html
 # Set stonith-timeout to the watchdog msgwait + 20%
 # stonith-timeout = Timeout (msgwait) + 20%
 vagrant ssh node-1 -c "sudo su - -c 'pcs property set stonith-timeout=24s'"
-vagrant ssh node-1 -c "sudo su - -c 'pcs stonith create MyStonith fence_sbd devices=/dev/sdb1 power_timeout=21 action=off'"
+vagrant ssh node-1 -c "sudo su - -c 'pcs property'"
 sleep 30
 
 # Locate the `active` node currently holding the MyStonith resource:
@@ -34,6 +35,7 @@ active=`vagrant ssh node-1 -c "sudo su - -c 'crm_resource -r MyStonith query --l
 passive=`python -c "import os; print({1: 2, 2: 1, 3: 1}[$active])"`
 
 # Test stonith:
+vagrant ssh node-$active -c "sudo su - -c 'uptime'"
 vagrant ssh node-$active -c "sudo su - -c 'pcs stonith show MyStonith'"
 vagrant ssh node-$active -c "sudo su - -c 'pcs cluster stop node-$active'"  # this should fence node-$active?
 sleep 30
